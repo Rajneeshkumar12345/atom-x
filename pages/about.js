3ba28cbb389033634c331cabbff19c906c2fcb91
Rajneeshkumar12345 <rkv7861999@gmail.com>
2022-09-29 01:04:13 +0530

    Add

--- pages/about.js
+++ pages/about.js
@@ -6,7 +6,7 @@ const about = () => {
         <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-EVSTQN3/azprG1Anm3QDgpJLIm9Nao0Yz1ztcQTwFspd3yD65VohhpuuCOmLASjC" crossorigin="anonymous"/>
         <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/js/bootstrap.bundle.min.js" integrity="sha384-MrcW6ZMFYlzcLA8Nl+NtUVF0sA7MsXsP1UyJoMp4YLEuNSfAP+JcXn/tWtIaxVXM" crossorigin="anonymous"></script>
 
-            <img src="/About1.jpg" height="500px" width="auto" style={{objectFit:"cover" }} padding="50px" class="card-img-top" alt="..."/>
+            <img src="/About1.jpg" height="500px" display="block" width="100%" object-fit="cover" object-position="bottom" class="card-img-top" alt="image"/>
             <div class="card-body" style={{textAlign:"centre", width:"42rem", margin:"auto"}}>
                 <div class="card-text"  style={{textAlign:"centre", marginTop:"50px", fontFamily:"sans-serif", fontSize:"20px"}} >
                 <p>Data shows that almost 80% of the entrepreneurs are not able to launch their 1st version of the idea due to the lack of technical knowledge or high amount of money needed to build the 1st version of their app/website.</p>
